a459741de7d35362cf56159fe05fe2c5aa69b7f8
HeD_pE <hed_pe@netcabo.pt>
2017-01-20 07:52:08 +0000

    7.0alpha11 Fix crash generating Troubleshoot Template for site with no INCs or CRQs

--- appCore/Templates/UI/TroubleshootControls.cs
+++ appCore/Templates/UI/TroubleshootControls.cs
@@ -887,11 +887,11 @@ namespace appCore.Templates.UI
 							else
 								tempDt.Rows.Add(type, dr["Change Ref"], dr["Summary"], dr["Status"], dr["Scheduled Start"], dr["Scheduled End"]);
 						}
+						DataView dv = tempDt.DefaultView;
+						dv.Sort = "Start Date desc";
+						tempDt = dv.ToTable();
+						currentCases.AddRange(tempDt.AsEnumerable());
 					}
-					DataView dv = tempDt.DefaultView;
-					dv.Sort = "Start Date desc";
-					tempDt = dv.ToTable();
-					currentCases.AddRange(tempDt.AsEnumerable());
 				}
 			}
 			return currentCases;
