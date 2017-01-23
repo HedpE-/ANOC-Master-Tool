6951cc9783cfa4e145f01cc3ba47ad6b46ef26cc
HeD_pE <hed_pe@netcabo.pt>
2017-01-23 08:48:38 +0000

    7.0alpha11 AlarmParser last fixes

--- appCore/Netcool/AlarmsParser.cs
+++ appCore/Netcool/AlarmsParser.cs
@@ -50,40 +50,44 @@ namespace appCore.Netcool
 				}
 				if(string.IsNullOrWhiteSpace(line))
 					continue;
-				
-				Alarm al = new Alarm(line.Split('\t'), headers);
-				AlarmsList.Add(al);
-				if(outage) {
-					if(al.Bearer == "4G" && !al.COOS)
-						lteSitesOnM.Add(al.SiteId);
+				Alarm al = null;
+				try {
+					al = new Alarm(line.Split('\t'), headers);
+				}catch {
+					continue;
 				}
-				else {
-					if(generateOutput) {
-						try {
-							parsedOutput += al;
-							if(line != temparr.Last())
-								parsedOutput += Environment.NewLine + Environment.NewLine;
-						}
-						catch {
-							continue;
+					AlarmsList.Add(al);
+					if(outage) {
+						if(al.Bearer == "4G" && !al.COOS && al.OnM)
+							lteSitesOnM.Add(al.SiteId);
+					}
+					else {
+						if(generateOutput) {
+							try {
+								parsedOutput += al;
+								if(line != temparr.Last())
+									parsedOutput += Environment.NewLine + Environment.NewLine;
+							}
+							catch {
+								continue;
+							}
 						}
 					}
 				}
-			}
-			
-			if(lteSitesOnM.Any())
-				lteSitesOnM = lteSitesOnM.Distinct().ToList();
+				
+				if(lteSitesOnM.Any())
+					lteSitesOnM = lteSitesOnM.Distinct().ToList();
 //			st.Stop();
 //			var t = st.Elapsed;
-		}
-		
-		public Outage GenerateOutage() {
-			return new Outage(this);
-		}
-		
-		public override string ToString()
-		{
-			return parsedOutput;
+			}
+			
+			public Outage GenerateOutage() {
+				return new Outage(this);
+			}
+			
+			public override string ToString()
+			{
+				return parsedOutput;
+			}
 		}
 	}
-}
