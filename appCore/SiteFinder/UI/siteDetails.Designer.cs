13d7d716decff36bb99199a43be0fe9638a46e32
HeD_pE <hed_pe@netcabo.pt>
2017-03-09 13:49:14 +0000

    7.1alpha Migrate listView2 to dataGridView on siteDetails

--- appCore/SiteFinder/UI/siteDetails.Designer.cs
+++ appCore/SiteFinder/UI/siteDetails.Designer.cs
@@ -27,7 +27,6 @@ namespace appCore.SiteFinder.UI
 		private appCore.UI.AMTTextBox textBox7;
 		private System.Windows.Forms.Label label3;
 		private appCore.UI.AMTTextBox textBox6;
-		private System.Windows.Forms.ListView listView1;
 		private System.Windows.Forms.Label label6;
 		private appCore.UI.AMTTextBox textBox5;
 		private System.Windows.Forms.Label label7;
@@ -54,6 +53,9 @@ namespace appCore.SiteFinder.UI
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.CheckBox checkBox4;
 		private System.Windows.Forms.CheckBox checkBox5;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.CheckBox checkBox6;
+		private System.Windows.Forms.CheckBox checkBox7;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -90,7 +92,6 @@ namespace appCore.SiteFinder.UI
 			this.textBox5 = new appCore.UI.AMTTextBox();
 			this.label7 = new System.Windows.Forms.Label();
 			this.textBox8 = new appCore.UI.AMTTextBox();
-			this.listView1 = new System.Windows.Forms.ListView();
 			this.checkBox1 = new System.Windows.Forms.CheckBox();
 			this.checkBox2 = new System.Windows.Forms.CheckBox();
 			this.checkBox3 = new System.Windows.Forms.CheckBox();
@@ -117,7 +118,11 @@ namespace appCore.SiteFinder.UI
 			this.button1 = new System.Windows.Forms.Button();
 			this.checkBox4 = new System.Windows.Forms.CheckBox();
 			this.checkBox5 = new System.Windows.Forms.CheckBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.checkBox6 = new System.Windows.Forms.CheckBox();
+			this.checkBox7 = new System.Windows.Forms.CheckBox();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// textBox4
@@ -243,20 +248,6 @@ namespace appCore.SiteFinder.UI
 			this.textBox8.Size = new System.Drawing.Size(34, 20);
 			this.textBox8.TabIndex = 91;
 			// 
-			// listView1
-			// 
-			this.listView1.AutoArrange = false;
-			this.listView1.FullRowSelect = true;
-			this.listView1.GridLines = true;
-			this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-			this.listView1.Location = new System.Drawing.Point(5, 247);
-			this.listView1.Name = "listView1";
-			this.listView1.Size = new System.Drawing.Size(556, 488);
-			this.listView1.TabIndex = 3;
-			this.listView1.UseCompatibleStateImageBehavior = false;
-			this.listView1.View = System.Windows.Forms.View.Details;
-			this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ListView1KeyDown);
-			// 
 			// checkBox1
 			// 
 			this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
@@ -271,7 +262,7 @@ namespace appCore.SiteFinder.UI
 			this.checkBox1.Text = "2G";
 			this.checkBox1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
 			this.checkBox1.UseVisualStyleBackColor = true;
-			this.checkBox1.CheckedChanged += new System.EventHandler(this.listboxFilter_Changed);
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.dataGridViewFilter_Changed);
 			// 
 			// checkBox2
 			// 
@@ -287,7 +278,7 @@ namespace appCore.SiteFinder.UI
 			this.checkBox2.Text = "3G";
 			this.checkBox2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
 			this.checkBox2.UseVisualStyleBackColor = true;
-			this.checkBox2.CheckedChanged += new System.EventHandler(this.listboxFilter_Changed);
+			this.checkBox2.CheckedChanged += new System.EventHandler(this.dataGridViewFilter_Changed);
 			// 
 			// checkBox3
 			// 
@@ -303,7 +294,7 @@ namespace appCore.SiteFinder.UI
 			this.checkBox3.Text = "4G";
 			this.checkBox3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
 			this.checkBox3.UseVisualStyleBackColor = true;
-			this.checkBox3.CheckedChanged += new System.EventHandler(this.listboxFilter_Changed);
+			this.checkBox3.CheckedChanged += new System.EventHandler(this.dataGridViewFilter_Changed);
 			// 
 			// label3
 			// 
@@ -528,11 +519,62 @@ namespace appCore.SiteFinder.UI
 			this.checkBox5.UseVisualStyleBackColor = true;
 			this.checkBox5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PaknetVodapageCheckBoxesMouseUp);
 			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.AllowUserToResizeColumns = false;
+			this.dataGridView1.AllowUserToResizeRows = false;
+			this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+			this.dataGridView1.Location = new System.Drawing.Point(5, 247);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersVisible = false;
+			this.dataGridView1.Size = new System.Drawing.Size(556, 488);
+			this.dataGridView1.TabIndex = 148;
+			this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
+			// 
+			// checkBox6
+			// 
+			this.checkBox6.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox6.Checked = true;
+			this.checkBox6.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.checkBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.checkBox6.Location = new System.Drawing.Point(523, 230);
+			this.checkBox6.Name = "checkBox6";
+			this.checkBox6.Size = new System.Drawing.Size(38, 18);
+			this.checkBox6.TabIndex = 150;
+			this.checkBox6.Text = "TF";
+			this.checkBox6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.checkBox6.UseVisualStyleBackColor = true;
+			this.checkBox6.CheckedChanged += new System.EventHandler(this.dataGridViewFilter_Changed);
+			// 
+			// checkBox7
+			// 
+			this.checkBox7.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox7.Checked = true;
+			this.checkBox7.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.checkBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.checkBox7.Location = new System.Drawing.Point(485, 230);
+			this.checkBox7.Name = "checkBox7";
+			this.checkBox7.Size = new System.Drawing.Size(38, 18);
+			this.checkBox7.TabIndex = 149;
+			this.checkBox7.Text = "VF";
+			this.checkBox7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.checkBox7.UseVisualStyleBackColor = true;
+			this.checkBox7.CheckedChanged += new System.EventHandler(this.dataGridViewFilter_Changed);
+			// 
 			// siteDetails
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1074, 741);
+			this.Controls.Add(this.listView2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.checkBox6);
+			this.Controls.Add(this.checkBox7);
 			this.Controls.Add(this.checkBox5);
 			this.Controls.Add(this.checkBox4);
 			this.Controls.Add(this.amtTextBox3);
@@ -545,7 +587,6 @@ namespace appCore.SiteFinder.UI
 			this.Controls.Add(this.label8);
 			this.Controls.Add(this.label13);
 			this.Controls.Add(this.label11);
-			this.Controls.Add(this.listView2);
 			this.Controls.Add(this.textBox7);
 			this.Controls.Add(this.textBox8);
 			this.Controls.Add(this.label10);
@@ -560,7 +601,6 @@ namespace appCore.SiteFinder.UI
 			this.Controls.Add(this.label7);
 			this.Controls.Add(this.checkBox2);
 			this.Controls.Add(this.checkBox1);
-			this.Controls.Add(this.listView1);
 			this.Controls.Add(this.label6);
 			this.Controls.Add(this.textBox5);
 			this.Controls.Add(this.label1);
@@ -580,6 +620,7 @@ namespace appCore.SiteFinder.UI
 			this.Text = "Site Details";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SiteDetailsFormClosing);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
