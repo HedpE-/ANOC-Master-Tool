a4a97a2056a62fab6b5d0db1fd65725d0daf5b19
HeD_pE <hed_pe@netcabo.pt>
2017-03-16 03:57:06 +0000

    7.0alpha12 OIConnection adaptations to meet new OI API changes Code cleanup

--- appCore/OI/OIConnection.cs
+++ appCore/OI/OIConnection.cs
@@ -210,55 +210,45 @@ namespace appCore.OI
 		/// <summary>
 		/// Requests data from OI APIs
 		/// </summary>
-		/// <param name="API">"cells", "incidents", "changes", "visits", "alarms", "availability", "access"</param>
+		/// <param name="API">sites-information", "cells", "incidents", "changes", "visits", "alarms", "availability", "access"</param>
 		/// <param name="site">Site number</param>
 		/// <param name="bearer">Bearer</param>
 		public static string requestApiOutput(string API, string site, int bearer = 4)
 		{
-			InitiateOiConnection();
-			if(LoggedOn) {
-				client.BaseUrl = new Uri("http://operationalintelligence.vf-uk.corp.vodafone.com");
-				client.CookieContainer = OICookieContainer;
-				client.Proxy = Proxy;
-				IRestRequest request = new RestRequest(string.Format("/api/sitelopedia/get-{0}", API), Method.POST);
-				request.AddParameter("siteNumbers", site);
-				request.AddParameter("range", string.Empty);
-				request.AddParameter("bearer", bearer);
-				IRestResponse response = client.Execute(request);
-				
-				return response.Content;
-			}
-			return string.Empty;
+//			InitiateOiConnection();
+//			if(LoggedOn) {
+//				client.BaseUrl = new Uri("http://operationalintelligence.vf-uk.corp.vodafone.com");
+//				client.CookieContainer = OICookieContainer;
+//				client.Proxy = Proxy;
+//				IRestRequest request = new RestRequest(string.Format("/api/sitelopedia/get-{0}", API), Method.POST);
+//				request.AddParameter("siteNumbers", site);
+//				request.AddParameter("range", string.Empty);
+//				request.AddParameter("bearer", bearer);
+//				IRestResponse response = client.Execute(request);
+//				
+//				return response.Content;
+//			}
+			List<string> siteList = new List<string>();
+			siteList.Add(site);
+			return requestApiOutput(API, siteList, bearer);
 		}
 		
 		/// <summary>
-		/// Requests data from OI APIs
+		/// Requests bulk data from OI APIs
 		/// </summary>
-		/// <param name="API">"cells", "incidents", "changes", "visits", "alarms", "availability", "access"</param>
+		/// <param name="API">"sites-information", "cells", "incidents", "changes", "visits", "alarms", "availability", "access"</param>
 		/// <param name="site">Site number</param>
 		/// <param name="bearer">Bearer</param>
 		public static string requestApiOutput(string API, IEnumerable<string> sitesList, int bearer = 4)
 		{
 			InitiateOiConnection();
 			if(LoggedOn) {
-//				HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("http://operationalintelligence.vf-uk.corp.vodafone.com" + string.Format("/api/sitelopedia/get-{0}", API));
-//				webRequest.Method = "POST";
-//				webRequest.CookieContainer = OICookieContainer;
-//
-//				string request = "siteNumbers=" + string.Join(",", sitesList) + "&range=&bearer=4";
-//				byte[] dataStream =  System.Text.Encoding.ASCII.GetBytes(request);
-//				webRequest.ContentLength = dataStream.Length;   //Count bytes to send
-//				var os = webRequest.GetRequestStream();
-//				os.Write(dataStream,0,dataStream.Length);
-//				os.Close();
-//				WebResponse webResponse = webRequest.GetResponse();
-				
 				client.BaseUrl = new Uri("http://operationalintelligence.vf-uk.corp.vodafone.com");
 				client.CookieContainer = OICookieContainer;
 				client.Proxy = Proxy;
 				client.Timeout = 300000;
 				IRestRequest request = new RestRequest(string.Format("/api/sitelopedia/get-{0}", API), Method.POST);
-				request.RequestFormat = DataFormat.Xml;
+//				request.RequestFormat = DataFormat.Xml;
 				request.AddParameter("siteNumbers", string.Join(",", sitesList));
 				request.AddParameter("range", string.Empty);
 				request.AddParameter("bearer", bearer);
@@ -316,7 +306,7 @@ namespace appCore.OI
 		/// <summary>
 		/// Requests data from OI PHP files
 		/// </summary>
-		/// <param name="phpFile">"inc", "crq", "alarms", "ca"</param>
+		/// <param name="phpFile">"inc", "crq", "visits", "alarms", "ca"</param>
 		/// <param name="site">Site number</param>
 		public static string requestPhpOutput(string phpFile, string site)
 		{
@@ -464,79 +454,5 @@ namespace appCore.OI
 			}
 			return string.Empty;
 		}
-		
-//			                        	I use the system.windows.forms.webbrowser and
-//
-		//httprequest / response in tandem to achieve what your looking for take the below:
-//
-		////navigate to the a page first with the webBrowser.
-//
-		//add the below to the documentcomplete event
-//
-		////
-//
-		////here I extract the cookies, but unfortunately the winforms webbrowser gives the cookies as one large string,
-//
-		////so you have to split it inorder to build a list / collection of system.net.Cookie
-//
-		//var c = webBrowser1.Document.Cookie.Split(";".ToCharArray());
-		////cookie split
-		//            var cookiecol = new CookieCollection();
-//
-		//            foreach (var cook in c)
-		//            {
-//
-		//             // cookie split again for name value pairs
-//
-		//                var name = cook.Trim().Split("=".ToCharArray())[0];
-		//                var value = cook.Trim().Split("=".ToCharArray())[1];
-		//                cookiecol.Add(new Cookie(name, value));
-		//            }
-		//            //I create a request
-		//            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(new Uri("http://***.com.au/game_results_download.php?date="+intialdate.ToString("dd/MM/yyyy")+"&game=1&num=550&state=qld"));
-//
-		//            req.CookieContainer = new CookieContainer();
-//
-		//           //attach the cookies
-//
-		//           req.CookieContainer.Add(new Uri("http://***.com.au"),cookiecol);
-//
-		//            var resp = (HttpWebResponse)req.GetResponse();
-//
-		//            // pull the data the string t - is actually a CSV file. in this case in my code base. but it could be anything.
-		//            string t = new StreamReader(resp.GetResponseStream(), Encoding.Default).ReadToEnd();
-//
-		//            /*The only way to negate the save file dialog is by with send keys. which is crap.
-//
-		//   The example above gets the file stream, even though I don't actually know the default name of the file that has been served.
-//
-		//*/
-//
-		//I hope this helps.
-//
-		//The file will typically be requested as a GET this means alittle bit of manual Browser Debug is going to go a long way to finding the url, for the download, I'd suggest firefox with firebug installed.
-		
-//			                        	HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://operationalintelligence.vf-uk.corp.vodafone.com/site/allsites.php");
-//
-//			                        	HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-//			                        	StreamReader input = new StreamReader(response.GetResponseStream());
-//
-//			                        	DataSet dsTest = new DataSet();
-//			                        	dsTest.ReadXml(input);
-//
-//			                        	siteDetailsTable = Functions.GetDataTableFromCsv(all_sites,true);
-//
-//			                        	string search = all_sites;
-//			                        	string text;
-//
-//			                        	//create the constructor with post type and few data
-//			                        	appCore.Tools.MyWebRequest myRequest = new Tools.MyWebRequest("http://www.yourdomain.com","POST","a=value1&b=value2");
-//			                        	//show the response string on the console screen.
-//			                        	Console.WriteLine(myRequest.GetResponse());
-//
-//			                        	using (var client = new WebClient())
-//			                        	{
-//			                        		text = client.DownloadString("http://operationalintelligence.vf-uk.corp.vodafone.com/site/allsites.php");
-//			                        	}
 	}
 }
