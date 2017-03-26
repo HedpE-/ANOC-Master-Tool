6561c2c26db87b8cb1ec5ccd30255c5448fc8ade
HeD_pE <hed_pe@netcabo.pt>
2017-03-26 22:39:18 +0100

    7.1alpha Relocated Months enum

--- appCore/Toolbox/Tools.cs
+++ appCore/Toolbox/Tools.cs
@@ -419,3 +419,18 @@ namespace appCore.Toolbox
 		}
 	}
 }
+
+public enum Months : byte {
+	January,
+	February,
+	March,
+	April,
+	May,
+	June,
+	July,
+	August,
+	September,
+	October,
+	November,
+	December
+};
