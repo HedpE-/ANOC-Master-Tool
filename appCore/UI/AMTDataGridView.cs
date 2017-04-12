656a88dc9da6659fa0224d7977e822af5af376f8
HeD_pE <hed_pe@netcabo.pt>
2017-04-12 16:26:09 +0100

    7.1alpha Disable AutoSelect on DataSourceChange

--- appCore/UI/AMTDataGridView.cs
+++ appCore/UI/AMTDataGridView.cs
@@ -30,7 +30,10 @@ namespace appCore.UI
 			set { DoubleBuffered = value; }
 		}
 		
+		private bool SuppressAutoSelection { get; set; }
+		
 		public AMTDataGridView() {
+			SuppressAutoSelection = true;
 			this.VerticalScrollBar.VisibleChanged += VerticalScrollBar_VisibleChanged;
 			this.VerticalScrollBar.SizeChanged += VerticalScrollBar_SizeChanged;
 		}
@@ -52,6 +55,50 @@ namespace appCore.UI
 			VerticalScrollBar.Height = Height - 2;
 			VerticalScrollBar.SetBounds(VerticalScrollBar.Location.X, VerticalScrollBar.Location.Y, VerticalScrollBar.Width, VerticalScrollBar.Height);
 			this.VerticalScrollBar.SizeChanged += VerticalScrollBar_SizeChanged;
+		}
+		
+		public new /*shadowing*/ object DataSource
+		{
+			get
+			{
+				return base.DataSource;
+			}
+			set
+			{
+				SuppressAutoSelection = true;
+				Form parent = this.FindForm();
+
+				// Either the selection gets cleared on form load....
+				parent.Load -= parent_Load;
+				parent.Load += parent_Load;
+
+				base.DataSource = value;
+
+				// ...or it gets cleared straight after the DataSource is set
+				ClearSelectionAndResetSuppression();
+			}
+		}
+
+		protected override void OnSelectionChanged(EventArgs e)
+		{
+			if (SuppressAutoSelection)
+				return;
+
+			base.OnSelectionChanged(e);
+		}
+
+		void ClearSelectionAndResetSuppression()
+		{
+			if (this.SelectedRows.Count > 0 || this.SelectedCells.Count > 0)
+			{
+				this.ClearSelection();
+				SuppressAutoSelection = false;
+			}
+		}
+
+		void parent_Load(object sender, EventArgs e)
+		{
+			ClearSelectionAndResetSuppression();
 		}
 	}
 }
