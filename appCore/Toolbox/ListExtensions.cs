86aeb4d455ad5b565ed90084a1a1f34c0ca7eb4b
HeD_pE <hed_pe@netcabo.pt>
2017-06-05 16:55:41 +0100

    7.1alpha3 ListExtensions

---
+++ appCore/Toolbox/ListExtensions.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: GONCARJ3
+ * Date: 06/01/2017
+ * Time: 20:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ListExtensions
+{
+    public static bool Contains(this List<string> list, string pattern, StringComparison stringComparison)
+    {
+        return list.FindIndex(x => x.Equals(pattern, stringComparison)) != -1;
+    }
+    
+    public static List<OpenWeatherAPI.WeatherItem> ConvertToWeatherItems(this List<OpenWeatherAPI.WeatherQuery> list)
+    {
+        var newList = list.Select(w => new OpenWeatherAPI.WeatherItem(w, w.name));
+        return newList.ToList();
+    }
+}
