8ebb60ad5b141cda11a6cd109b0ea0783ac23597
HeD_pE <hed_pe@netcabo.pt>
2017-10-09 03:15:13 +0100

    8.1 -New namespace for all templates

--- appCore/Templates/Template.cs
+++ appCore/Templates/Template.cs
@@ -7,7 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using appCore.Templates.Types;
+using appCore.Templates.RAN.Types;
 using appCore.Settings;
 
 namespace appCore.Templates
