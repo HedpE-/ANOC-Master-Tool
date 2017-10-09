9db85a09d79dfb82733dbdc86bfa78d2ab85c98d
HeD_pE <hed_pe@netcabo.pt>
2017-10-09 03:14:16 +0100

    8.1 -Implemented ErrorProvider -Confluence logon fixes

--- appCore/Web/UI/AuthForm.cs
+++ appCore/Web/UI/AuthForm.cs
@@ -22,40 +22,86 @@ namespace appCore.Web.UI
 		public string Username = string.Empty;
 		public string Password = string.Empty;
 
-        string application; 
+        string application;
+
+        appCore.UI.ErrorProviderFixed errorProvider = new appCore.UI.ErrorProviderFixed();
 		//private string lastUserLoginAttempt = string.Empty;
 		//private int loginFailedAttempts = 0;
 		
 		public AuthForm(string app)
-		{
-			InitializeComponent();
-
+        {
             application = app;
-		}
+
+            InitializeComponent();
+
+            if (application == "Confluence")
+            {
+                ComboBox userComboBox = new ComboBox()
+                {
+                    Location = textBox1.Location,
+                    Name = "UserNameComboBox",
+                    Size = textBox1.Size
+                };
+                userComboBox.Items.Add(Settings.CurrentUser.PT.Email);
+                userComboBox.Items.Add(Settings.CurrentUser.UK.Email);
+                Controls.Remove(textBox1);
+                Controls.Add(userComboBox);
+            }
+
+            errorProvider.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+            errorProvider.SetIconPadding(textBox1, -17);
+            errorProvider.SetIconPadding(textBox2, -17);
+        }
 		
 		void Button1Click(object sender, EventArgs e)
-		{
-			label3.Text = "Login failed";
-			label3.Visible = false;
-			if(!(string.IsNullOrEmpty(textBox1.Text) && string.IsNullOrEmpty(textBox2.Text)))
+        {
+            string username = string.Empty;
+            if (application == "OI")
             {
-				Username = textBox1.Text;
-				Password = application == "OI" ? textBox2.Text.EncryptText() : textBox2.Text;
-				
+                errorProvider.SetError(textBox1, string.Empty);
+                username = textBox1.Text;
+            }
+            else
+            {
+                errorProvider.SetError(Controls["UserNameComboBox"], string.Empty);
+                username = Controls["UserNameComboBox"].Text;
+            }
+            errorProvider.SetError(textBox2, string.Empty);
+
+            label3.Text = "Login failed";
+			label3.Visible = false;
+
+			if(!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(textBox2.Text))
+            {
+				Username = username;
+				Password = application == "OI" ? textBox2.Text.Encrypt() : textBox2.Text;
+
+                DialogResult = DialogResult.OK;
 				Close();
 			}
 			else
             {
-				label3.Text = "No credentials entered";
+				label3.Text = "Invalid credentials";
 				label3.Visible = true;
-			}
+                if (string.IsNullOrEmpty(username))
+                    errorProvider.SetError(application == "OI" ? textBox1 : Controls["UserNameComboBox"], "Please enter a valid username");
+                if (string.IsNullOrEmpty(textBox2.Text))
+                    errorProvider.SetError(textBox2, "Please enter a valid password");
+            }
 		}
 		
 		void Button2Click(object sender, EventArgs e)
 		{
 			textBox1.Text = string.Empty;
+            try
+            {
+                Controls["UserNameComboBox"].Text = string.Empty;
+            }
+            catch { }
 			textBox2.Text = string.Empty;
-			Close();
+
+            DialogResult = DialogResult.Cancel;
+            Close();
 		}
 
 		public bool validateLogin(string url)
@@ -77,10 +123,12 @@ namespace appCore.Web.UI
 			request.ContentLength = postBytes.Length;
 			Stream dataStream = request.GetRequestStream ();
 			dataStream.Write(postBytes, 0, postBytes.Length);
-			try {
+			try
+            {
 				WebResponse response = request.GetResponse();
 			}
-			catch (Exception) {
+			catch (Exception)
+            {
 				return false;
 			}
 			
@@ -96,7 +144,7 @@ namespace appCore.Web.UI
             }
 
             textBox1.Text = application == "OI" ? Settings.SettingsFile.OIUsername : Username;
-            textBox2.Text = application == "OI" ? Settings.SettingsFile.OIPassword.DecryptText() : string.Empty;
+            textBox2.Text = application == "OI" ? Settings.SettingsFile.OIPassword.Decrypt() : string.Empty;
             Text = application == "OI" ? "OI Login" : "Confluence Login";
         }
     }
