b9052c4c91622130ccf4f5ca0833603689d9f6f1
HeD_pE <hed_pe@netcabo.pt>
2017-10-09 03:48:15 +0100

    8.1 -Old gif spinner replaced with CircularProgressBar

--- appCore/UI/LoadingPanel.cs
+++ appCore/UI/LoadingPanel.cs
@@ -11,46 +11,105 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Windows.Forms;
+using Transitions;
 
 namespace appCore.UI
 {
 	/// <summary>
 	/// Description of LoadingPanel.
 	/// </summary>
-	public class LoadingPanel
+	public class LoadingPanel : Panel
 	{
-        Panel innerPanel;
+        //Panel innerPanel;
 
-        PictureBox loadingBox;
+        //PictureBox loadingBox;
+        CircularProgressBar.CircularProgressBar loadingBox;
 
         Control ParentControl;
 
-        public int SpinnerSize
-        {
-            get;
-            set;
-        } = 32;
-        
-		public double Opacity
+        static System.Windows.Forms.Timer tOut = new System.Windows.Forms.Timer();
+
+        public double Opacity
 		{
             get;
             set;
-        } = 0.4;
+        } = 0.8;
 
-        public bool Visible
+        public bool IsVisible
         {
             get
             {
-                return innerPanel != null;
+                return Visible;
             }
         }
 
-        public bool LoadingSpinnerVisible
+        public bool IsLoadingSpinnerVisible
         {
             get
             {
-                return innerPanel.Controls.Contains(loadingBox);
+                return Controls.Contains(loadingBox);
             }
+        }
+
+        public bool ShowLoadingSpinner
+        {
+            get;
+            set;
+        }
+
+        public int LoadingSpinnerSize
+        {
+            get;
+            set;
+        } = 96;
+
+        ProgressBarStyle loadingSpinnerStyle = ProgressBarStyle.Marquee;
+        public ProgressBarStyle LoadingSpinnerStyle
+        {
+            get
+            {
+                return loadingSpinnerStyle;
+            }
+            set
+            {
+                loadingSpinnerStyle = value;
+                if (loadingBox != null)
+                    loadingBox.Style = loadingSpinnerStyle;
+            }
+        }
+
+        int progressValue;
+        public int LoadingSpinnerProgressValue
+        {
+            get
+            {
+                return progressValue;
+            }
+            set
+            {
+                progressValue = value;
+                if (loadingBox != null)
+                {
+                    if (loadingBox.InvokeRequired)
+                        loadingBox.Invoke((MethodInvoker)delegate
+                        {
+                            loadingBox.Value = progressValue;
+                            loadingBox.Text = progressValue.ToString();
+                        });
+                    else
+                    {
+                        loadingBox.Value = progressValue;
+                        loadingBox.Text = progressValue.ToString();
+                    }
+                }
+            }
+        }
+
+        public LoadingPanel()
+        {
+            tOut.Enabled = false;
+            tOut.Interval = 25;
+            tOut.Tick += FadeOutTimerTick;
         }
 
         /// <summary>
@@ -63,30 +122,28 @@ namespace appCore.UI
         {
             if (parentControl.Parent != null)
             {
-                if (parentControl.Parent.Controls.Cast<Control>().Any(c => c.GetType() == typeof(LoadingPanel)))
+                if (parentControl.Parent.Controls.OfType<LoadingPanel>().Any())
                     throw new Exception("Another Loading Panel already exists in Parent.");
             }
             else
             {
-                if (parentControl.Controls.Cast<Control>().Any(c => c.GetType() == typeof(LoadingPanel)))
+                if (parentControl.Controls.OfType<LoadingPanel>().Any())
                     throw new Exception("Another Loading Panel already exists in Control.");
             }
 
             ParentControl = parentControl;
-
-            innerPanel = new Panel();
+            
 			darkenBackgroundForm(showLoading);
         }
 
         public void Close()
         {
-            if (innerPanel != null)
+            if (Parent != null)
             {
-                if (innerPanel.Parent != null)
-                {
-                    innerPanel.Parent.Controls.Remove(innerPanel);
-                    innerPanel.Dispose();
-                }
+                //FadeOut();
+
+                Parent.Controls.Remove(this);
+                Dispose();
             }
         }
 
@@ -98,13 +155,13 @@ namespace appCore.UI
             {
                 if (ParentControl is AMTMenuStrip)
                 {
-                    parentForm.Controls.Add(innerPanel);
-                    innerPanel.Location = parentForm.PointToClient(ParentControl.Parent.PointToScreen(ParentControl.Location));
+                    parentForm.Controls.Add(this);
+                    Location = parentForm.PointToClient(ParentControl.Parent.PointToScreen(ParentControl.Location));
                 }
                 else
                 {
-                    ParentControl.Parent.Controls.Add(innerPanel);
-                    innerPanel.Location = ParentControl.Location;
+                    ParentControl.Parent.Controls.Add(this);
+                    Location = ParentControl.Location;
                 }
             }
             else
@@ -112,12 +169,12 @@ namespace appCore.UI
                 if (ParentControl.InvokeRequired)
                     ParentControl.Invoke((MethodInvoker)delegate
                     {
-                        ParentControl.Controls.Add(innerPanel);
+                        ParentControl.Controls.Add(this);
                     });
                 else
-                    ParentControl.Controls.Add(innerPanel);
-
-                innerPanel.Location = Point.Empty;
+                    ParentControl.Controls.Add(this);
+                
+                Location = Point.Empty;
             }
             // take a screenshot of the form and darken it
             Bitmap bmp;
@@ -144,39 +201,143 @@ namespace appCore.UI
             if (ParentControl.InvokeRequired)
                 ParentControl.Invoke((MethodInvoker)delegate
                 {
-                    innerPanel.Size = ParentControl.ClientRectangle.Size;
+                    Size = ParentControl.ClientRectangle.Size;
                 });
             else
-                innerPanel.Size = ParentControl.ClientRectangle.Size;
-            innerPanel.BackgroundImage = bmp;
+                Size = ParentControl.ClientRectangle.Size;
+            BackgroundImage = bmp;
 
-            innerPanel.BringToFront();
+            BringToFront();
             if (showLoading)
                 ToggleLoadingSpinner();
         }
 
         public void ToggleLoadingSpinner()
         {
-            if (LoadingSpinnerVisible)
+            if (IsLoadingSpinnerVisible)
             {
-                innerPanel.Controls.Remove(loadingBox);
+                Controls.Remove(loadingBox);
                 loadingBox.Dispose();
             }
             else
             {
-                Point loc = innerPanel.PointToScreen(Point.Empty);
-                loc.X = (ParentControl.Width - SpinnerSize) / 2;
-                loc.Y = (ParentControl.Height - SpinnerSize) / 2;
+                Point loc = PointToScreen(Point.Empty);
+                loc.X = (ParentControl.Width - LoadingSpinnerSize) / 2;
+                loc.Y = (ParentControl.Height - LoadingSpinnerSize) / 2;
 
-                PictureBox loadingBox = new PictureBox();
-                loadingBox.BackColor = Color.Transparent;
-                loadingBox.Image = loadingBox.InitialImage = Resources.spinner1;
-                loadingBox.Size = new Size(SpinnerSize, SpinnerSize);
-                loadingBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                loadingBox = new CircularProgressBar.CircularProgressBar();
+
+                loadingBox.Size = new Size(LoadingSpinnerSize, LoadingSpinnerSize);
                 loadingBox.Location = loc;
-                innerPanel.Controls.Add(loadingBox);
+                loadingBox.Style = LoadingSpinnerStyle;
+                loadingBox.Value = loadingSpinnerStyle == ProgressBarStyle.Marquee ? 15 : LoadingSpinnerProgressValue;
+
+                loadingBox.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+                loadingBox.Anchor = AnchorStyles.None;
+                loadingBox.AnimationSpeed = 500;
+                loadingBox.BackColor = Color.Transparent;
+                loadingBox.ForeColor = Color.White;
+                loadingBox.InnerColor = Color.White;
+                loadingBox.InnerMargin = 0;
+                loadingBox.InnerWidth = 0;
+                loadingBox.MarqueeAnimationSpeed = 2000;
+                loadingBox.OuterColor = Color.Black;
+                loadingBox.OuterMargin = -12;
+                loadingBox.OuterWidth = 15;
+                if(Settings.CurrentUser.UserName == "GONCARJ3")
+                    loadingBox.ProgressColor = Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(151)))), ((int)(((byte)(218)))));
+                else
+                    loadingBox.ProgressColor = Color.DarkRed;
+                loadingBox.ProgressWidth = 12;
+                loadingBox.SecondaryFont = new Font("Microsoft Sans Serif", 11.125F);
+                loadingBox.StartAngle = 270;
+                //loadingBox.SubscriptColor = Color.Gray;
+                //loadingBox.SubscriptMargin = new Padding(0);
+                loadingBox.SubscriptText = "";
+                if (loadingSpinnerStyle != ProgressBarStyle.Marquee)
+                {
+                    loadingBox.SuperscriptColor = Color.LightGray;
+                    loadingBox.SuperscriptMargin = new Padding(3, 22, 0, 0);
+                    loadingBox.SuperscriptText = "%";
+                }
+                else
+                    loadingBox.SuperscriptText = "";
+                loadingBox.Font = new Font("Arial", ResolveLoadingSpinnerTextSize(), FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
+                loadingBox.Text = loadingSpinnerStyle == ProgressBarStyle.Marquee ? "PLEASE WAIT" : LoadingSpinnerProgressValue.ToString();
+                loadingBox.TextMargin = loadingSpinnerStyle == ProgressBarStyle.Marquee ? new Padding(0, 2, 0, 0) : new Padding(0, 2, 0, 0);
+                
+                Controls.Add(loadingBox);
+
+                //CircularProgressBar.CircularProgressBar circularProgressBar1 = null;
+                //if (loadingSpinnerStyle != ProgressBarStyle.Marquee)
+                //{
+                //    circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+                //    circularProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+                //    circularProgressBar1.AnimationSpeed = 500;
+                //    circularProgressBar1.BackColor = System.Drawing.Color.Transparent;
+                //    circularProgressBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                //    circularProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+                //    circularProgressBar1.InnerColor = System.Drawing.Color.Transparent;
+                //    circularProgressBar1.InnerMargin = 2;
+                //    circularProgressBar1.InnerWidth = -1;
+                //    circularProgressBar1.Location = loc;
+                //    circularProgressBar1.MarqueeAnimationSpeed = 2000;
+                //    circularProgressBar1.Name = "circularProgressBar1";
+                //    circularProgressBar1.OuterColor = System.Drawing.Color.Black;
+                //    circularProgressBar1.OuterMargin = -10;
+                //    circularProgressBar1.OuterWidth = 5;
+                //    circularProgressBar1.ProgressColor = System.Drawing.Color.DarkRed;
+                //    circularProgressBar1.ProgressWidth = 18;
+                //    circularProgressBar1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+                //    circularProgressBar1.Size = new System.Drawing.Size(LoadingSpinnerSize, LoadingSpinnerSize);
+                //    circularProgressBar1.StartAngle = 270;
+                //    circularProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+                //    circularProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+                //    circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+                //    circularProgressBar1.SubscriptText = "";
+                //    circularProgressBar1.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+                //    circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+                //    circularProgressBar1.SuperscriptText = "";
+                //    circularProgressBar1.TabIndex = 16;
+                //    circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+                //    circularProgressBar1.Value = 2;
+
+                //    Controls.Add(circularProgressBar1);
+                //}
+
                 loadingBox.BringToFront();
+                //if (loadingSpinnerStyle != ProgressBarStyle.Marquee)
+                //    circularProgressBar1.BringToFront();
             }
+        }
+
+        float ResolveLoadingSpinnerTextSize()
+        {
+            if(loadingBox!=null)
+            {
+                float size = 0;
+                if (loadingBox.Style == ProgressBarStyle.Marquee)
+                    return 5.75F;
+                else
+                    return 17.75F;
+            }
+
+            return 0;
+        }
+
+        void FadeOutTimerTick(object sender, EventArgs e)
+        {
+            if (this.Opacity > 0)
+                this.Opacity -= 0.075;
+            else
+                tOut.Enabled = false;
+        }
+
+        void FadeOut()
+        {
+            tOut.Enabled = true;
+
+            //while (tOut.Enabled) { }
         }
     }
 }
